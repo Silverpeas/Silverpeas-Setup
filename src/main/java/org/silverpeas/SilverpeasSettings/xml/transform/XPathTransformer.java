dc9479d55f10c4d798897c09765a17401a81a6b7
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2012-10-17 15:42:08 +0200

    Updating licensing

--- src/main/java/org/silverpeas/SilverpeasSettings/xml/transform/XPathTransformer.java
+++ src/main/java/org/silverpeas/SilverpeasSettings/xml/transform/XPathTransformer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2000 - 2009 Silverpeas
+ * Copyright (C) 2000 - 2012 Silverpeas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -11,7 +11,7 @@
  * Open Source Software ("FLOSS") applications as described in Silverpeas's
  * FLOSS exception.  You should have received a copy of the text describing
  * the FLOSS exception, and it is also available here:
- * "http://repository.silverpeas.com/legal/licensing"
+ * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -84,6 +84,7 @@ public class XPathTransformer implements XmlTransformer {
       in = new BufferedInputStream(new FileInputStream(xmlFile));
       DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
       docFactory.setValidating(false);
+      docFactory.setIgnoringElementContentWhitespace(false);
       DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
       docBuilder.setEntityResolver(new ClasspathEntityResolver(null));
       doc = docBuilder.parse(in);
