75e7ee628cde9e1af88b9455d2ea4f799477f4e4
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2025-03-20 17:48:01 +0100

    Add a new dependency scope for the silverpeas.gradle file in a Silverpeas installation: custom. This scope allow to declare libs developed specifically to custom some behaviors or to provide additional custom functionalities (like workflows).

--- src/main/groovy/org/silverpeas/setup/SoftwareBundles.groovy
+++ src/main/groovy/org/silverpeas/setup/SoftwareBundles.groovy
@@ -13,36 +13,51 @@ import javax.inject.Inject
  * unpacked to a given directory in order to generate the final application.
  * @author mmoquillon
  */
+@SuppressWarnings('unused')
 class SoftwareBundles {
 
-  /**
-   * All the software bundles that made Silverpeas. Those bundles are usually downloaded from our
-   * own Software Repository by the Silverpeas installer. They are required to assemble and build
-   * the final Silverpeas Web Application. The Jar libraries other than the supported JDBC drivers
-   * aren't taken in charge.
-   */
-  @InputFiles
-  final ConfigurableFileCollection silverpeas
+    /**
+     * All the software bundles that made Silverpeas. Those bundles are usually downloaded from our
+     * own Software Repository by the Silverpeas installer. They are required to assemble and build
+     * the final Silverpeas Web Application. The Jar libraries other than the supported JDBC drivers
+     * aren't taken in charge.
+     */
+    @InputFiles
+    final ConfigurableFileCollection silverpeas
 
-  /**
-   * Any tiers bundles to add into the Silverpeas Application being built. The tiers bundles are
-   * processed differently by the plugin: only the JAR libraries are taken in charge.
-   */
-  @InputFiles
-  final ConfigurableFileCollection tiers
+    /**
+     * Any tiers bundles to add into the Silverpeas Application being built. The tiers bundles are
+     * processed differently by the plugin: only the JAR libraries are taken in charge.
+     */
+    @InputFiles
+    final ConfigurableFileCollection tiers
 
-  @Inject
-  SoftwareBundles(Project project) {
-    silverpeas = project.files()
-    tiers = project.files()
-  }
+    /**
+     * Custom software bundles to complete or to add custom functionalities to Silverpeas. Those
+     * bundles are usually downloaded from our own or outer Software Repositories by the
+     * Silverpeas installer. The external repositories require to be declared. This is a way to
+     * include libraries dedicated to implement a customer requirement (like a workflow process for
+     * example).
+     */
+    @InputFiles
+    final ConfigurableFileCollection custom
 
-  void setSilverpeas(FileCollection bundles) {
-    this.silverpeas.setFrom(bundles)
-  }
+    @Inject
+    SoftwareBundles(Project project) {
+        silverpeas = project.files()
+        tiers = project.files()
+        custom = project.files()
+    }
 
-  @SuppressWarnings('unused')
-  void setTiers(FileCollection bundles) {
-    this.tiers.setFrom(bundles)
-  }
+    void setSilverpeas(FileCollection bundles) {
+        this.silverpeas.setFrom(bundles)
+    }
+
+    void setTiers(FileCollection bundles) {
+        this.tiers.setFrom(bundles)
+    }
+
+    void setCustom(FileCollection bundles) {
+        this.custom.setFrom(bundles)
+    }
 }
